23fcdb81e678e4e1ee129f4e649ade5797c37958
chandanpadhan00 <142711074+chandanpadhan00@users.noreply.github.com>
2024-10-17 16:43:17 +0530

    Update string2.sql

--- string2.sql
+++ string2.sql
@@ -3,12 +3,12 @@ SELECT
     -- Condition 1: Both |TermA and |TermB present
     WHEN INSTR(PRSTN_STR_VAL_R, '|') > 0 AND INSTR(PRSTN_STR_VAL_R, '/') > 0 THEN 
       SUBSTR(PRSTN_STR_VAL_R, 1, INSTR(PRSTN_STR_VAL_R, '|') - 1) || ' / ' ||
-      SUBSTR(PRSTN_STR_VAL_R, INSTR(PRSTN_STR_VAL_R, '/') + 2)
+      SUBSTR(PRSTN_STR_VAL_R, INSTR(PRSTN_STR_VAL_R, '/') + 2, INSTR(PRSTN_STR_VAL_R, '|', 1, 2) - INSTR(PRSTN_STR_VAL_R, '/') - 2)
     
     -- Condition 2: Only |TermA is present, no |TermB
     WHEN INSTR(PRSTN_STR_VAL_R, '|') > 0 THEN
       SUBSTR(PRSTN_STR_VAL_R, 1, INSTR(PRSTN_STR_VAL_R, '|') - 1) || ' / ' || 
-      SUBSTR(PRSTN_STR_VAL_R, INSTR(PRSTN_STR_VAL_R, '|', 1, 1) + 1)
+      SUBSTR(PRSTN_STR_VAL_R, INSTR(PRSTN_STR_VAL_R, '/') + 2)
 
     -- Condition 3: No pipes, return as is
     ELSE 
